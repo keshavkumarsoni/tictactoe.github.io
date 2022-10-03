f75edc7ffb8c7d7b93ee9664e012a25524013979
keshavkumarsoni <sonikeshavghiloth420@gmail.com>
2022-10-03 12:53:31 +0530

    document
    document

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Welcome To Tic Tac Toe Made By Keshav Kumar Soni")
-let music=new Audio("music.mp3");
+let music = new Audio("music.mp3");
 let audioTurn = new Audio("ting.mp3");
-let gameover=new Audio("gameover.mp3");
+let gameover = new Audio("gameover.mp3");
 let turn = "X";
 let isGameOver = false;
 
@@ -49,9 +49,9 @@ Array.from(boxes).forEach(element => {
 
             if (!isGameOver) {
                 document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
-                
+
             }
-            
+
 
 
         }
